4cec89be6a85c49871936152ef0b2e4a6244c296
Karen Wong <karen.wsywong@gmail.com>
2015-06-24 13:06:37 -0700

    create linked_list.py

---
+++ linked_list.py
@@ -0,0 +1,28 @@
+class LinkList():
+
+    def insert(val):
+        """ will insert the value 'val' at the head of the list"""
+        pass
+
+    def pop():
+        """will pop the first value off the head of the list and return it"""
+        pass
+
+    def size():
+        """will return the length of the list"""
+        pass
+
+    def search(val):
+        """
+        will return the node containing 'val' in the list, if present, else None
+
+        """
+        pass
+
+    def remove(node):
+        """will remove the given node from the list, wherever it might be (node must be an item in the list"""
+        pass
+
+    def display():
+        """will print the list represented as a Python tuple literal: "(12, 'sam', 37, 'tango')"""
+        pass
