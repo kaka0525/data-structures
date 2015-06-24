ca81e6910447fc93049b1290f32aff27e1c64e85
Karen Wong <karen.wsywong@gmail.com>
2015-06-24 16:22:25 -0700

    add test_linked_list.py

---
+++ test_linked_list.py
@@ -0,0 +1,29 @@
+def test_insert():
+    pass
+
+
+def test_pop():
+    li = LinkedList()
+    node1 = Node("Scott")
+    node2 = Node("Karen")
+    li.insert(node1)
+    li.insert(node2)
+    li.pop()
+    assert
+    assert
+
+
+def test_size():
+    pass
+
+
+def test_search():
+    pass
+
+
+def test_remove():
+    pass
+
+
+def test_display():
+    pass
