daa40b13ddb7a84e07a1e446f215b80b585a8042
Karen Wong <karen.wsywong@gmail.com>
2015-06-24 16:25:08 -0700

    add functions on linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,28 +1,84 @@
-class LinkList():
+class Node(object):
+    def __init__(self, data=None, pointer=None):
+        self.data = data
+        self.pointer = pointer
 
-    def insert(val):
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.pointer
+
+    def set_next(self, new_next):
+        self.pointer = new_next
+
+
+class LinkList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, val):
         """ will insert the value 'val' at the head of the list"""
-        pass
+        new_node = Node(val)
+        new_node.set_next(self.head)
+        self.head = new_node
 
-    def pop():
+    def pop(self):
         """will pop the first value off the head of the list and return it"""
-        pass
+        current = self.head
+        return current
+        self.head = current.get_next()
 
-    def size():
+    def size(self):
         """will return the length of the list"""
-        pass
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+        return count
 
-    def search(val):
+    def search(self, val):
         """
-        will return the node containing 'val' in the list, if present, else None
+        will return the node containing 'val' in the list, if present,
+        else None
 
         """
-        pass
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.get_data() == val:
+                found = True
+            else:
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not in list")
+        return current
 
-    def remove(node):
-        """will remove the given node from the list, wherever it might be (node must be an item in the list"""
-        pass
+    def remove(self, node):
+        """
+        will remove the given node from the list, wherever it might be
+        (node must be an item in the list
+        """
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.get_data() == node:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not in list")
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
 
-    def display():
-        """will print the list represented as a Python tuple literal: "(12, 'sam', 37, 'tango')"""
+    def display(self):
+        """will print the list represented as a Python tuple literal:
+        "(12, 'sam', 37, 'tango')
+            bonus if you can make it so that the list appears this way on its
+            own (remember special methods)."""
         pass
