f583fc3da12e184354e3b1630641631a5339d371
Scott Schmidt <sjschmidt44@gmail.com>
2015-06-24 18:00:04 -0700

    Add partial work from today's data structure assignment

--- linked_list.py
+++ linked_list.py
@@ -1,84 +1,53 @@
 class Node(object):
-    def __init__(self, data=None, pointer=None):
-        self.data = data
+    def __init__(self, val=None, pointer=None):
+        self.val = val
         self.pointer = pointer
+        print(self.val, self.pointer)
 
-    def get_data(self):
-        return self.data
+    def get_info(self):
+        return self.val
 
-    def get_next(self):
+    def next_node(self):
         return self.pointer
 
-    def set_next(self, new_next):
-        self.pointer = new_next
+    def set_next(self, next_n):
+        self.pointer = next_n
+
+    # def __repr__(self):
+    #     print(self.val)
 
 
 class LinkList(object):
-    def __init__(self, head=None):
+    def __init__(self, head=None, **kwargs):
         self.head = head
+        self.size = 0
+
+    def size(self):
+        return self.size
+
+    def search(self):
+        pass
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.val)
+            current = current.next_node()
+
+    def remove(self):
+        self.size += 1
+        pass
 
     def insert(self, val):
         """ will insert the value 'val' at the head of the list"""
         new_node = Node(val)
         new_node.set_next(self.head)
         self.head = new_node
+        self.size += 1
 
     def pop(self):
         """will pop the first value off the head of the list and return it"""
         current = self.head
+        self.head = current.next_node()
+        self.size += 1
         return current
-        self.head = current.get_next()
-
-    def size(self):
-        """will return the length of the list"""
-        current = self.head
-        count = 0
-        while current:
-            count += 1
-            current = current.get_next()
-        return count
-
-    def search(self, val):
-        """
-        will return the node containing 'val' in the list, if present,
-        else None
-
-        """
-        current = self.head
-        found = False
-        while current and found is False:
-            if current.get_data() == val:
-                found = True
-            else:
-                current = current.get_next()
-        if current is None:
-            raise ValueError("Data not in list")
-        return current
-
-    def remove(self, node):
-        """
-        will remove the given node from the list, wherever it might be
-        (node must be an item in the list
-        """
-        current = self.head
-        previous = None
-        found = False
-        while current and found is False:
-            if current.get_data() == node:
-                found = True
-            else:
-                previous = current
-                current = current.get_next()
-        if current is None:
-            raise ValueError("Data not in list")
-        if previous is None:
-            self.head = current.get_next()
-        else:
-            previous.set_next(current.get_next())
-
-    def display(self):
-        """will print the list represented as a Python tuple literal:
-        "(12, 'sam', 37, 'tango')
-            bonus if you can make it so that the list appears this way on its
-            own (remember special methods)."""
-        pass
--- test_linked_list.py
+++ test_linked_list.py
@@ -1,29 +1,39 @@
-def test_insert():
+from linked_list import LinkList
+
+
+def test_list_size():
+    li = LinkList('List')
+    li.insert('node1')
+    li.insert('node2')
+    li.insert('node3')
+    assert li.size is not None
+    assert li.size is 3
+
+
+def test_list_search():
     pass
 
 
-def test_pop():
-    li = LinkedList()
-    node1 = Node("Scott")
-    node2 = Node("Karen")
-    li.insert(node1)
-    li.insert(node2)
-    li.pop()
-    assert
-    assert
+def test_list_display():
+    li = LinkList('List')
+    li.insert('node1')
+    li.insert('node2')
+    li.insert('node3')
+    assert li.display == (u'node3', u'node2', u'node1')
 
 
-def test_size():
+def test_list_remove():
     pass
 
 
-def test_search():
-    pass
+def test_list_insert():
+    li = LinkList('List')
+    li.insert('node1')
+    li.insert('node2')
+    li.insert('node3')
+    assert li.head is not None
+    assert li.head.val is 'node3'
 
 
-def test_remove():
-    pass
-
-
-def test_display():
+def test_list_pop():
     pass
