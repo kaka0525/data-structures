fef59fda63126960f16be2df3cc87e4114756b77
Scott Schmidt <sjschmidt44@gmail.com>
2015-06-25 13:28:51 -0700

    Tests in progress. Writing methods for Stack push and pop.

--- stack.py
+++ stack.py
@@ -5,8 +5,10 @@ class Stack(object):
     def __init__(self, top=None):
         self.top = top
 
-    def push(self):
-        pass
+    def push(self, val):
+        ll = LinkList()
+        top_ = ll.insert(val)
+        self.top = top_
 
     def pop(self):
         pass
--- test_stack.py
+++ test_stack.py
@@ -3,8 +3,23 @@ from stack import Stack
 
 
 def test_push_to_stack():
-    pass
+    new_stack_push = Stack()
+    assert new_stack_push.top is None
+    new_stack_push.push('box1')
+    assert new_stack_push.top is not None
+    assert new_stack_push.top.val is 'box1'
+    new_stack_push.push('box2')
+    new_stack_push.push('box3')
+    assert new_stack_push.top.val is 'box3'
 
 
 def test_pop_from_stack():
-    pass
+    new_stack_pop = Stack()
+    assert new_stack_pop.top is None
+    new_stack_pop.push('box1')
+    new_stack_pop.push('box2')
+    assert new_stack_pop.top.val is 'box2'
+    new_stack_pop.pop()
+    assert new_stack_pop.top.val is 'box1'
+    with pytest.raises(TypeError):
+        pass  # Need to verify error
