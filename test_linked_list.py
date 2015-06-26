4ad14cdc8e6839da889b1d4005fc67a1f1726693
Karen Wong <karen.wsywong@gmail.com>
2015-06-25 22:42:05 -0700

    add test to examine constructor take iterable of values

--- test_linked_list.py
+++ test_linked_list.py
@@ -1,17 +1,17 @@
 import pytest
-from linked_list import LinkList
+from linked_list import LinkedList
 
 
 def test_list_size():
-    one_node = LinkList()
+    one_node = LinkedList()
     one_node.insert('node1')
-    many_nodes = LinkList()
+    many_nodes = LinkedList()
     many_nodes.insert('node1')
     many_nodes.insert('node2')
     many_nodes.insert('node3')
     many_nodes.insert('node4')
     many_nodes.insert('node5')
-    empty = LinkList()
+    empty = LinkedList()
     assert empty.size_ is 0
     assert one_node.size_ is 1
     assert many_nodes.size_ is 5
@@ -20,7 +20,7 @@ def test_list_size():
 
 
 def test_list_search():
-    many_nodes = LinkList()
+    many_nodes = LinkedList()
     many_nodes.insert('node1')
     many_nodes.insert('node2')
     many_nodes.insert('node3')
@@ -34,11 +34,11 @@ def test_list_search():
 
 
 def test_list_display():
-    li = LinkList()
+    li = LinkedList()
     li.insert('node1')
     li.insert('node2')
     li.insert('node3')
-    many_nodes = LinkList()
+    many_nodes = LinkedList()
     many_nodes.insert('node1')
     many_nodes.insert('node2')
     many_nodes.insert('node3')
@@ -46,19 +46,19 @@ def test_list_display():
     many_nodes.insert('node5')
     many_nodes.insert('node6')
     many_nodes.pop()
-    empty = LinkList()
+    empty = LinkedList()
     assert li.display() == "('node3','node2','node1')"
     assert many_nodes.display() == "('node5','node4','node3','node2','node1')"
     assert empty.display() == "()"
 
 
 def test_list_remove():
-    li = LinkList()
+    li = LinkedList()
     li.insert('node1')
     li.insert('node2')
     li.insert('node3')
     li.remove('node3')
-    many_nodes = LinkList()
+    many_nodes = LinkedList()
     many_nodes.insert('node1')
     many_nodes.insert('node2')
     many_nodes.insert('node3')
@@ -66,7 +66,7 @@ def test_list_remove():
     many_nodes.insert('node5')
     many_nodes.insert('node6')
     many_nodes.remove('node4')
-    one_node = LinkList()
+    one_node = LinkedList()
     one_node.insert('node1')
     one_node.remove('node1')
     assert li.size_ is 2
@@ -77,7 +77,7 @@ def test_list_remove():
 
 
 def test_list_insert():
-    li = LinkList()
+    li = LinkedList()
     li.insert('node1')
     li.insert('node2')
     li.insert('node3')
@@ -89,7 +89,7 @@ def test_list_insert():
 
 
 def test_list_pop():
-    many_nodes = LinkList()
+    many_nodes = LinkedList()
     many_nodes.insert('node1')
     many_nodes.insert('node2')
     many_nodes.insert('node3')
@@ -97,11 +97,16 @@ def test_list_pop():
     many_nodes.insert('node5')
     many_nodes.insert('node6')
     many_nodes.pop()
-    one_node = LinkList()
+    one_node = LinkedList()
     one_node.insert('node1')
     one_node.pop()
-    empty_node = LinkList()
+    empty_node = LinkedList()
     assert many_nodes.size_ is 5
     assert one_node.size_ is 0
     with pytest.raises(ValueError):
         empty_node.pop()
+
+
+def test_linked_list_constructor():
+    iterable_list = LinkedList([1, 2, 3])
+    assert iterable_list.display() == "(3,2,1)"
