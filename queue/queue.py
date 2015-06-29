771c5d03e2812fb58fbdb25a71acac1130215199
Scott Schmidt <sjschmidt44@gmail.com>
2015-06-29 14:08:28 -0700

    Add doc strings to methods.

--- queue/queue.py
+++ queue/queue.py
@@ -15,6 +15,13 @@ class Queue(object):
         self.size_ = 0
 
     def enqueue(self, val):
+        """
+        Insert new Node at the back of the queue. If queue is empty, first Node
+        becomes Front of queue. Otherwise, new Nodes are added to back of
+        queue.
+            arg:
+                val: Value associated as the data of the Node
+        """
         if self.front is None:
             node = Node(val)
             self.front = node
@@ -27,19 +34,15 @@ class Queue(object):
             self.size_ += 1
 
     def dequeue(self):
+        """
+        Remove Node from Front of queue, and assign next Node in line as
+        Front of queue.
+        """
         current = self.front
         self.front = current.next_node
         current.next_node = None
         self.size_ -= 1
 
     def size(self):
+        """Return the current amount of Nodes in the queue as size"""
         return self.size_
-
-
-"""
-1. enqueue(value): adds value to the queue
-2. dequeue(): removes the correct item from the queue and returns its value
-    (should raise an error if the queue is empty)
-3. size(): return the size of the queue.  Should return 0 if the queue is
-    empty.
-"""
