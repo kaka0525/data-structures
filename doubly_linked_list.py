ac18a1a2f57c546a916548a6fbe1a8870730db86
Scott Schmidt <sjschmidt44@gmail.com>
2015-06-30 14:50:27 -0700

    Add data strucutre, double linked list and associated tests, to repo.

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -1,7 +1,11 @@
+# _*_ coding: utf-8- _*_
+from __future__ import unicode_literals
+
+
 class Node(object):
-    def __init__(self, val=None, next=None, prev=None):
+    def __init__(self, val=None, next_node=None, prev=None):
         self.val = val
-        self.next = next
+        self.next_node = next_node
         self.prev = prev
 
 
@@ -10,33 +14,70 @@ class Doublylinkedlist(object):
         self.head = None
         self.tail = None
 
-
-        def insert(val):
-            """ Insert the value 'val' at the head of the list"""
+    def insert(self, val):
+        """ Insert the value 'val' at the head of the list"""
+        if self.head is None:
             new_node = Node(val)
-            Dummy.next = new_node
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node = Node(val)
+            new_node.next_node = self.head
+            self.head.prev = new_node
             self.head = new_node
 
+    def append(self, val):
+        """Append the value 'val' at the tail of the list"""
+        new_node = Node(val)
+        current = self.tail
+        if current is None:
+            self.tail, self.head = new_node
+            new_node.next_node, new_node.prev = new_node
+        else:
+            current.prev = new_node
+            new_node.next_node = current
+            self.tail = new_node
 
-        def append(val):
-            """Append the value 'val' at the tail of the list"""
-            pass
+    def pop(self):
+        """Pop the first value off the head of the list and return it"""
+        if self.head is None:
+            raise AssertionError("There is nothing to pop")
+        else:
+            pop_value = self.head.val
+            self.head = self.head.next_node
+            return pop_value
 
-        def pop():
-            """Pop the first value off the head of the list and return it"""
-            pass
+    def shift(self):
+        """Remove the tail value from the tail of the list and return it"""
+        current = self.tail
+        if self.tail is None:
+            raise ValueError("The list is empty")
+        elif self.head is current:
+            self.head = self.tail = None
+        else:
+            current.prev.next_node = None
+            self.tail = self.tail.prev
+        return current.val
 
-        def shift():
-            """Remove the last value from the tail of the list and return it"""
-            pass
-
-        def remove(val):
-            """
-            Remove the first instance of 'val' found in the list, starting from
-            the head. If 'val' is nor present, it will raise an appropriate
-            python exception
-            """
-            pass
-
-
-
+    def remove(self, val=None):
+        """
+        Remove the head instance of 'val' found in the list, starting from
+        the head. If 'val' is nor present, it will raise an appropriate
+        python exception
+        """
+        if not val:
+            raise ValueError("You must provide a Node Value")
+        if self.head is None:
+            raise AttributeError("The list is empty")
+        else:
+            current = self.head
+            while current.val != val:
+                if current.next_node is None:
+                    raise ValueError("Value not found in list")
+                else:
+                    current = current.next_node
+            if current.prev is None:
+                self.head = self.head.next_node
+            else:
+                current.prev.next_node = current.next_node
+                current.next_node.prev = current.prev
--- test_doubly_linked_list.py
+++ test_doubly_linked_list.py
@@ -1,3 +1,54 @@
-import pytest
 from doubly_linked_list import Doublylinkedlist
+import pytest
 
+
+@pytest.fixture()
+def list_setup():
+    dbl_list = Doublylinkedlist()
+    dbl_list.insert(1)
+    dbl_list.insert(2)
+    dbl_list.insert(3)
+    dbl_list.insert(4)
+    return dbl_list
+
+
+def test_empty_list():
+    dbl_list = Doublylinkedlist()
+    assert dbl_list.head is None
+    assert dbl_list.tail is None
+
+
+def test_insert_node_at_head(list_setup):
+    assert list_setup.head.val is 4
+    list_setup.insert(5)
+    assert list_setup.head.val is 5
+
+
+def test_append_node_at_tail(list_setup):
+    assert list_setup.tail.val is 1
+    list_setup.append(5)
+    list_setup.append(6)
+    assert list_setup.tail.val is 6
+
+
+def test_pop_node_at_head(list_setup):
+    assert list_setup.pop() == 4
+    list_setup.insert(5)
+    assert list_setup.pop() == 5
+
+
+def test_shift_node_at_tail(list_setup):
+    list_setup.shift()
+    assert list_setup.tail.val is 2
+    list_setup.shift()
+    assert list_setup.tail.val is 3
+
+
+def test_remove_node_from_list(list_setup):
+    pass
+
+
+def test_remove_no_argument(list_setup):
+    with pytest.raises(ValueError):
+        list_setup.remove()
+    assert ValueError
