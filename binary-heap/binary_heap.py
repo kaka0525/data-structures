04f854a9bfa5f5d182947ccfaea5ac7a99103124
Scott Schmidt <sjschmidt44@gmail.com>
2015-07-01 16:08:10 -0700

    Add functional and tested Push method to repo.

--- binary-heap/binary_heap.py
+++ binary-heap/binary_heap.py
@@ -1,5 +1,6 @@
 #  encode
 from __future__ import unicode_literals
+from math import ceil
 
 
 class MinBinaryHeap(object):
@@ -15,8 +16,23 @@ class MinBinaryHeap(object):
         """
         Puts a new value into the heap, maintaining the heap property.
         """
-        self.heap_list.append(val)
-
+        current = self.heap_list
+        current.append(val)
+        p_index = 0
+        if len(current) > 2:
+            idx = current.index(val)
+            p_index = int(ceil((idx / 2.0) - 1))
+            while val < current[p_index]:
+                current[idx], current[p_index] = current[p_index], current[idx]
+                idx = p_index
+                # p_index = int(ceil((idx / 2.0) - 1))
+        else:
+            if len(current) is 2:
+                if current[0] > current[1]:
+                    current[0], current[1] = current[1], current[0]
+            else:
+                return current
+        return current
 
     def pop(self):
         """
--- binary-heap/test_binary_heap.py
+++ binary-heap/test_binary_heap.py
@@ -10,15 +10,16 @@ def heap():
 
 def test_push_to_new_heap(heap):
     heap.push(18)
-    assert heap[4] is 18
+    assert heap.heap_list[4] is 18
     heap.push(2)
-    assert heap[1] is 2
+    assert heap.heap_list[4] is 2
+    assert heap.heap_list[-1] is 18
 
 
 def test_pop_from_heap(heap):
     heap.pop()
-    assert heap[0] is 4
-    assert heap[3] is 67
+    assert heap.heap_list[0] is 4
+    assert heap.heap_list[3] is 67
 
 
 def test_pop_from_empty_heap():
@@ -28,10 +29,10 @@ def test_pop_from_empty_heap():
 
 def test_push_iter_to_heap():
     bh = MinBinaryHeap([2, 24, 1, 48, 3, 50, 90, 67, 1000])
-    assert bh[0] is 1
-    assert bh[4] is # something
+    assert bh.heap_list[0] is 1
+    assert bh.heap_list[4] is 24
 
 
 def test_push_non_iter_to_heap():
     with pytest.raises(TypeError):
-        bh = MinBinaryHeap(['beans', 'bears', 'bottles'])
+        MinBinaryHeap(['beans', 'bears', 'bottles'])
