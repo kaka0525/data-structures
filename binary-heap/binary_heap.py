ecdd86980bfa098d09011c14ef2519aaa7dcb74f
Scott Schmidt <sjschmidt44@gmail.com>
2015-07-01 14:16:21 -0700

    Add tests to repo for binary heap.

--- binary-heap/binary_heap.py
+++ binary-heap/binary_heap.py
@@ -3,14 +3,20 @@ from __future__ import unicode_literals
 
 
 class MinBinaryHeap(object):
-    def __init__(self, valuesn=None):
-        pass
+    def __init__(self, values=None):
+        self.heap_list = []
+        if values is not None:
+            for value in values:
+                if type(value) is not int:
+                    raise TypeError("That is not a number value")
+                self.push(value)
 
     def push(self, val):
         """
         Puts a new value into the heap, maintaining the heap property.
         """
-        pass
+        self.heap_list.append(val)
+
 
     def pop(self):
         """
--- binary-heap/test_binary_heap.py
+++ binary-heap/test_binary_heap.py
@@ -3,21 +3,35 @@ import pytest
 
 
 @pytest.fixture()
-def heap_setup():
-    pass
+def heap():
+    bh = MinBinaryHeap([1, 4, 16, 22, 28, 34, 56, 67, 69])
+    return bh
 
 
-def test_push_to_new_heap(heap_setup):
-    pass
+def test_push_to_new_heap(heap):
+    heap.push(18)
+    assert heap[4] is 18
+    heap.push(2)
+    assert heap[1] is 2
 
 
-def test_pop_from_heap(heap_setup):
-    pass
+def test_pop_from_heap(heap):
+    heap.pop()
+    assert heap[0] is 4
+    assert heap[3] is 67
 
 
-def test_pop_from_empty_heap(heap_setup):
-    pass
+def test_pop_from_empty_heap():
+    with pytest.raises(IndexError):
+        heap.pop()
 
 
 def test_push_iter_to_heap():
-    pass
+    bh = MinBinaryHeap([2, 24, 1, 48, 3, 50, 90, 67, 1000])
+    assert bh[0] is 1
+    assert bh[4] is # something
+
+
+def test_push_non_iter_to_heap():
+    with pytest.raises(TypeError):
+        bh = MinBinaryHeap(['beans', 'bears', 'bottles'])
