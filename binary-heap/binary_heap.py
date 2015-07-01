55171af0c5cbd3dff2a76429e3be04dab4336bed
Scott Schmidt <sjschmidt44@gmail.com>
2015-07-01 13:46:57 -0700

    Add Binary Heap folder and files to branch.

---
+++ binary-heap/binary_heap.py
@@ -0,0 +1,19 @@
+#  encode
+from __future__ import unicode_literals
+
+
+class MinBinaryHeap(object):
+    def __init__(self, valuesn=None):
+        pass
+
+    def push(self, val):
+        """
+        Puts a new value into the heap, maintaining the heap property.
+        """
+        pass
+
+    def pop(self):
+        """
+        Removes the "top" value in the heap, maintaining the heap property.
+        """
+        pass
---
+++ binary-heap/test_binary_heap.py
@@ -0,0 +1,23 @@
+from binary_heap import MinBinaryHeap
+import pytest
+
+
+@pytest.fixture()
+def heap_setup():
+    pass
+
+
+def test_push_to_new_heap(heap_setup):
+    pass
+
+
+def test_pop_from_heap(heap_setup):
+    pass
+
+
+def test_pop_from_empty_heap(heap_setup):
+    pass
+
+
+def test_push_iter_to_heap():
+    pass
