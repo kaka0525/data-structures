7b23c11f61dea039ee20172ea4cd1aaf738739cd
Karen Wong <karen.wsywong@gmail.com>
2015-07-02 06:38:54 -0700

    add pop function

--- binary-heap/binary_heap.py
+++ binary-heap/binary_heap.py
@@ -3,6 +3,14 @@ from __future__ import unicode_literals
 from math import ceil
 
 
+def _get_left_child_index(i):
+    return 2 * i + 1
+
+
+def _get_right_child_index(i):
+    return 2 * i + 2
+
+
 class MinBinaryHeap(object):
     def __init__(self, values=None):
         self.heap_list = []
@@ -20,44 +28,59 @@ class MinBinaryHeap(object):
         current.append(val)
         p_index = 0
         if len(current) > 2:
-            idx = current.index(val)
+            idx = len(current) - 1
             p_index = int(ceil((idx / 2.0) - 1))
             while val < current[p_index]:
                 current[idx], current[p_index] = current[p_index], current[idx]
                 idx = p_index
                 if p_index > 1:
-                    p_index = int(ceil((idx / 2) - 1))
+                    p_index = int(ceil((idx / 2.0) - 1))
                 else:
                     p_index = 0
         else:
-            if len(current) is 2:
+            if len(current) is 2.0:
                 if current[0] > current[1]:
                     current[0], current[1] = current[1], current[0]
             else:
                 return current
         return current
 
+    def _swap_nodes(self, a_index, b_index):
+        current = self.heap_list
+        (current[a_index], current[b_index]) = (current[b_index],
+                                                current[a_index])
+
     def pop(self):
         """
         Removes the "top" value in the heap, maintaining the heap property.
         """
         current = self.heap_list
-        try:
-            current[0], current[-1] = current[-1], current[0]
-            current.pop()
-        except:
-            try:
-                current.pop()
-            except IndexError:
-                raise IndexError('The list is empty.')
-        idx = 0
-        c_idx = (idx * 2) + 1
-        import pdb; pdb.set_trace()
-        while current[idx] > current[c_idx]:
-            current[idx], current[c_idx] = current[c_idx], current[idx]
-            idx = c_idx
-            if len(current) + 1 < c_idx:
-                c_idx = (idx * 2) + 1
-            else:
-                break
-        return current
+        if len(current) == 0:
+            raise IndexError("The heap is empty")
+        else:
+            return_val = current[0]
+            current[0] = current[-1]
+            del current[-1]
+            max_index = len(current) - 1
+            current_index = 0
+            while True:
+                left_child_index = _get_left_child_index(current_index)
+                right_child_index = _get_right_child_index(current_index)
+
+                if left_child_index > max_index:
+                    break
+                elif (right_child_index > max_index and
+                        current[left_child_index] < current[current_index]):
+                    self._swap_nodes(current_index, left_child_index)
+                    break
+                elif (current[left_child_index] <= current[right_child_index] and
+                        current[left_child_index] < current[current_index]):
+                    self._swap_nodes(current_index, left_child_index)
+                    current_index = left_child_index
+                elif (current[right_child_index] < current[left_child_index] and
+                        current[right_child_index] < current[current_index]):
+                    self._swap_nodes(current_index, right_child_index)
+                    current_index = right_child_index
+                else:
+                    break
+            return return_val
