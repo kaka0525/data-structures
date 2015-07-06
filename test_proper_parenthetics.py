64fd14c4a416496dd542402eedad1fe27fd99f67
Karen Wong <karen.wsywong@gmail.com>
2015-07-05 19:14:48 -0700

    add more tests and seperate assert statements

--- test_proper_parenthetics.py
+++ test_proper_parenthetics.py
@@ -2,15 +2,27 @@ import pytest
 from proper_parenthetics import proper_parenthetics
 
 
-def test_proper_parenthetics():
+def test_balanced_parenthetics():
     assert proper_parenthetics('()') == 0
-    assert proper_parenthetics('') == 0
-    assert proper_parenthetics(')(') == -1
-    assert proper_parenthetics('())') == -1
-    assert proper_parenthetics('(') == 1
-    assert proper_parenthetics(')') == -1
-    assert proper_parenthetics('))))))((((((') == -1
-    assert proper_parenthetics('()(') == 1
-    assert proper_parenthetics('()()()()()()()(') == 1
-    assert proper_parenthetics('()()()()()()())') == -1
     assert proper_parenthetics('(((((((((())))))))))') == 0
+
+
+def test_empty_parenthetics():
+    assert proper_parenthetics('') == 0
+    assert proper_parenthetics('         ') == 0
+
+
+def test_broken_parenthetics():
+    assert proper_parenthetics('))))))((((((') == -1
+    assert proper_parenthetics('()()()()()()())') == -1
+
+
+def test_open_parenthetics():
+    assert proper_parenthetics('((((((((()))))))))(') == 1
+    assert proper_parenthetics('((((((((()))))))))((((')
+
+
+def test_odd_input():
+    assert proper_parenthetics('(hello((((((world)))))))') == 0
+    assert proper_parenthetics(')!(((!!!!!!!)))!)') == -1
+    assert proper_parenthetics('(#((((((#()))#))))#)((') == 1
